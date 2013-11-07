7f10953f02b5063e7832dbd4916133d8761c65a1
thatAndyMoore <arpmoore@gmail.com>
2013-11-07 16:02:25 -0500

    Added basic parameters and constructor to 'Hole' class.

--- eclipseClientProject/GolfrClient/src/golfCourseObjects/Hole.java
+++ eclipseClientProject/GolfrClient/src/golfCourseObjects/Hole.java
@@ -2,4 +2,18 @@ package golfCourseObjects;
 
 public class Hole {
 
+	public Integer par;
+	public Integer redTeeYardage;
+	public Integer whiteTeeYargage;
+	public Integer blueTeeYardage;
+	public Integer score;
+	
+	public Hole(Integer parIn, Integer redTeeYardageIn, Integer whiteTeeYardageIn, Integer blueTeeYardageIn, Integer scoreIn)
+	{
+		this.par = parIn;
+		this.redTeeYardage = redTeeYardageIn;
+		this.whiteTeeYargage = whiteTeeYardageIn;
+		this.blueTeeYardage = blueTeeYardageIn;
+		this.score = scoreIn;
+	}
 }
