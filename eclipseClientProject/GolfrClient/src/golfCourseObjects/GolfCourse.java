1877b2f054f727676db094d1523f010e70f4a1de
thatAndyMoore <arpmoore@gmail.com>
2013-11-07 16:13:18 -0500

    Added parameters and a constructor to the GolfCourse class

--- eclipseClientProject/GolfrClient/src/golfCourseObjects/GolfCourse.java
+++ eclipseClientProject/GolfrClient/src/golfCourseObjects/GolfCourse.java
@@ -1,5 +1,23 @@
 package golfCourseObjects;
 
+import java.util.ArrayList;
+
 public class GolfCourse {
 
+	public String streetName;
+	public String streetNumber;
+	public String postalCode;
+	public String phoneNumber;
+	public String email;
+	public ArrayList<Hole> holes;
+	
+	public GolfCourse(String streetNameIn, String streetNumberIn, String postalCodeIn, String phoneNumberIn, String emailIn, ArrayList<Hole> holesIn)
+	{
+		this.streetName = streetNameIn;
+		this.streetNumber = streetNumberIn;
+		this.postalCode = postalCodeIn;
+		this.phoneNumber = phoneNumberIn;
+		this.email = emailIn;
+		this.holes = holesIn;
+	}
 }
