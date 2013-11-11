fca72a3bf3b37ad2666161a00ff74a721e5905ba
mgisoni <mgisoni@gmail.com>
2013-11-10 19:29:52 -0500

    Refactor of golfCourseObject classes to use private variables with getters and setters

--- eclipseClientProject/GolfrClient/src/golfCourseObjects/GolfCourse.java
+++ eclipseClientProject/GolfrClient/src/golfCourseObjects/GolfCourse.java
@@ -2,22 +2,117 @@ package golfCourseObjects;
 
 import java.util.ArrayList;
 
-public class GolfCourse {
+public class GolfCourse 
+{
 
-	public String streetName;
-	public String streetNumber;
-	public String postalCode;
-	public String phoneNumber;
-	public String email;
-	public ArrayList<Hole> holes;
-	
-	public GolfCourse(String streetNameIn, String streetNumberIn, String postalCodeIn, String phoneNumberIn, String emailIn, ArrayList<Hole> holesIn)
+	private String streetName;
+	private String streetNumber;
+	private String postalCode;
+	private String phoneNumber;
+	private String email;
+	private ArrayList<Hole> holes;
+
+	/**
+	 * Default constructor 
+	 */
+	public GolfCourse() 
 	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public GolfCourse(String streetNameIn, String streetNumberIn,
+			String postalCodeIn, String phoneNumberIn, String emailIn,
+			ArrayList<Hole> holesIn) {
 		this.streetName = streetNameIn;
 		this.streetNumber = streetNumberIn;
 		this.postalCode = postalCodeIn;
 		this.phoneNumber = phoneNumberIn;
 		this.email = emailIn;
 		this.holes = holesIn;
+	}
+
+	/**
+	 * @return the streetName
+	 */
+	public String getStreetName() {
+		return this.streetName;
+	}
+
+	/**
+	 * @param streetName the streetName to set
+	 */
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	/**
+	 * @return the streetNumber
+	 */
+	public String getStreetNumber() {
+		return this.streetNumber;
+	}
+
+	/**
+	 * @param streetNumber the streetNumber to set
+	 */
+	public void setStreetNumber(String streetNumber) {
+		this.streetNumber = streetNumber;
+	}
+
+	/**
+	 * @return the postalCode
+	 */
+	public String getPostalCode() {
+		return this.postalCode;
+	}
+
+	/**
+	 * @param postalCode the postalCode to set
+	 */
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return this.email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the holes
+	 */
+	public ArrayList<Hole> getHoles() {
+		return this.holes;
+	}
+
+	/**
+	 * @param holes the holes to set
+	 */
+	public void setHoles(ArrayList<Hole> holes) {
+		this.holes = holes;
 	}
 }
--- eclipseClientProject/GolfrClient/src/golfCourseObjects/Hole.java
+++ eclipseClientProject/GolfrClient/src/golfCourseObjects/Hole.java
@@ -1,19 +1,135 @@
 package golfCourseObjects;
 
+/**
+ * The Hole class represents a hole on a golf course.
+ * @author Matt Gisoni
+ *
+ */
 public class Hole {
 
-	public Integer par;
-	public Integer redTeeYardage;
-	public Integer whiteTeeYargage;
-	public Integer blueTeeYardage;
-	public Integer score;
+	private Integer par;
+	private Integer redTeeYardage;
+	private Integer whiteTeeYargage;
+	private Integer blueTeeYardage;
+	private Integer score;
+	private Integer handicap;
+
+	/**
+	 * Default Constructor.
+	 */
+	public Hole()
+	{
+		this.par = 0;
+		this.redTeeYardage = 0;
+		this.whiteTeeYargage = 0;
+		this.blueTeeYardage = 0;
+		this.score = 0;
+		this.handicap = 0;
+	}
 	
-	public Hole(Integer parIn, Integer redTeeYardageIn, Integer whiteTeeYardageIn, Integer blueTeeYardageIn, Integer scoreIn)
+	/**
+	 * Full constructor to populate all class variables.
+	 * @param parIn
+	 * @param redTeeYardageIn
+	 * @param whiteTeeYardageIn
+	 * @param blueTeeYardageIn
+	 * @param scoreIn
+	 * @param handicap
+	 */
+	public Hole(Integer parIn, Integer redTeeYardageIn,
+			Integer whiteTeeYardageIn, Integer blueTeeYardageIn,
+			Integer scoreIn, Integer handicap) 
 	{
 		this.par = parIn;
 		this.redTeeYardage = redTeeYardageIn;
 		this.whiteTeeYargage = whiteTeeYardageIn;
 		this.blueTeeYardage = blueTeeYardageIn;
 		this.score = scoreIn;
+		this.handicap = handicap;
 	}
+
+	/**
+	 * @return the par
+	 */
+	public Integer getPar() {
+		return this.par;
+	}
+
+	/**
+	 * @param par the par to set
+	 */
+	public void setPar(Integer par) {
+		this.par = par;
+	}
+
+	/**
+	 * @return the redTeeYardage
+	 */
+	public Integer getRedTeeYardage() {
+		return this.redTeeYardage;
+	}
+
+	/**
+	 * @param redTeeYardage the redTeeYardage to set
+	 */
+	public void setRedTeeYardage(Integer redTeeYardage) {
+		this.redTeeYardage = redTeeYardage;
+	}
+
+	/**
+	 * @return the whiteTeeYargage
+	 */
+	public Integer getWhiteTeeYargage() {
+		return this.whiteTeeYargage;
+	}
+
+	/**
+	 * @param whiteTeeYargage the whiteTeeYargage to set
+	 */
+	public void setWhiteTeeYargage(Integer whiteTeeYargage) {
+		this.whiteTeeYargage = whiteTeeYargage;
+	}
+
+	/**
+	 * @return the blueTeeYardage
+	 */
+	public Integer getBlueTeeYardage() {
+		return this.blueTeeYardage;
+	}
+
+	/**
+	 * @param blueTeeYardage the blueTeeYardage to set
+	 */
+	public void setBlueTeeYardage(Integer blueTeeYardage) {
+		this.blueTeeYardage = blueTeeYardage;
+	}
+
+	/**
+	 * @return the score
+	 */
+	public Integer getScore() {
+		return this.score;
+	}
+
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
+	/**
+	 * @return the handicap
+	 */
+	public Integer getHandicap() {
+		return this.handicap;
+	}
+
+	/**
+	 * @param handicap the handicap to set
+	 */
+	public void setHandicap(Integer handicap) {
+		this.handicap = handicap;
+	}
+	
 }
