0105e3a75666add080a86ffd295892991cc5dea9
thatAndyMoore <arpmoore@gmail.com>
2013-11-16 17:07:13 -0500

    Course list screen now shows a sample list of courses and is able to navigate to hole info entry screen

--- eclipseClientProject/GolfrClient/src/com/golfrclient/CourseListScreen.java
+++ eclipseClientProject/GolfrClient/src/com/golfrclient/CourseListScreen.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import controller.SelectACourse;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -25,14 +27,30 @@ public class CourseListScreen extends Activity {
 
 		courseListView = (ListView) findViewById(R.id.CourseListScreen_CourseList);
 		addCourseButton = (Button) findViewById(R.id.CourseListScreen_AddCourseButton);
-/*
+		
+		/*
+		 * Populate the list of courses
+		 *  <<< THIS JUST USES DUMMY INFO RIGHT NOW, WIRE UP TO CONTROLLER >>>
+		 */
 		ArrayList<String> courseNameList = new ArrayList<String>();
 		courseNameList.add("Oakmont");
 		courseNameList.add("Nevilewood");
-		ArrayAdapter<String> courseNamesArrayAdapter = new ArrayAdapter<String>(CourseListScreen.this,
-				R.layout.activity_course_list_screen, courseNameList);
+		ArrayAdapter<String> courseNamesArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, courseNameList);
 		courseListView.setAdapter(courseNamesArrayAdapter);
-		*/
+		
+		/*
+		 * Button to navigate to course creation screen
+		 */
+		addCourseButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(CourseListScreen.this, HoleInfoEntryScreen.class);
+				startActivity(i);
+				
+			}
+		});
+		
 	}
 
 	@Override
