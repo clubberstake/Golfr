6141d73f7e9dbdb17c631f1144b2a7195278d7b0
thatAndyMoore <arpmoore@gmail.com>
2013-11-17 19:06:59 -0500

    Basic piping for hole entry screen

--- eclipseClientProject/GolfrClient/src/com/golfrclient/HoleInfoEntryScreen.java
+++ eclipseClientProject/GolfrClient/src/com/golfrclient/HoleInfoEntryScreen.java
@@ -3,13 +3,28 @@ package com.golfrclient;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HoleInfoEntryScreen extends Activity {
+
+	private EditText whiteTeeYardEntry;
+	private EditText redTeeYardEntry;
+	private EditText blueTeeYardEntry;
+	private EditText parEntry;
+	private Button nextButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hole_info_entry_screen);
+
+		whiteTeeYardEntry = (EditText) findViewById(R.id.HoleInfoScreen_WhiteTeeEntry);
+		redTeeYardEntry = (EditText) findViewById(R.id.HoleInfoScreen_RedTeeEntry);
+		blueTeeYardEntry = (EditText) findViewById(R.id.HoleInfoScreen_BlueTeeEntry);
+		parEntry = (EditText) findViewById(R.id.HoleInfoScreen_ParEntry);
+		nextButton = (Button) findViewById(R.id.HoleInfoScreen_NextButton);
+
 	}
 
 	@Override
