2e07c697b388e1a03b5a75daa293d37233c2d597
thatAndyMoore <arpmoore@gmail.com>
2013-12-03 15:34:15 -0500

    Created a class HoleFetcher to test AsyncTask functionally with holelistscreen

---
+++ eclipseClientProject/GolfrClient/src/controller/HoleFetcher.java
@@ -0,0 +1,40 @@
+package controller;
+
+import golfCourseObjects.GolfCourse;
+import golfCourseObjects.Hole;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class HoleFetcher  extends SQLQueries{
+	private ArrayList<Hole> holeList;
+	private GolfCourse course;
+	
+	public HoleFetcher(){
+		super();
+	}
+	
+	public HoleFetcher(GolfCourse course){
+		super();
+		this.course = course;
+	}
+	
+	private void getHoleListFromDB(){
+		super.connect();
+		try{
+			this.holeList = super.getHoleMetadata(course);
+		}
+		catch (SQLException e){
+			e.printStackTrace();
+		}
+		finally{
+			this.close();
+		}
+	}
+	
+	public ArrayList getHoleList() throws SQLException
+	{
+		getCourseListFromDB();
+		return holeList;
+	}
+
+}
