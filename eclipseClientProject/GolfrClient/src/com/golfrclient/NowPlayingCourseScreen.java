82e058077457b4e3b43eadd65e5cd7d6a15e3e7d
thatAndyMoore <arpmoore@gmail.com>
2013-12-05 19:22:55 -0500

    Began to wire up Now Playing Screen

--- eclipseClientProject/GolfrClient/src/com/golfrclient/NowPlayingCourseScreen.java
+++ eclipseClientProject/GolfrClient/src/com/golfrclient/NowPlayingCourseScreen.java
@@ -1,15 +1,37 @@
 package com.golfrclient;
 
+import golfCourseObjects.Game;
+import golfCourseObjects.GolfCourse;
+import controller.MasterController;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class NowPlayingCourseScreen extends Activity {
-
+	
+	private ListView currentlyPlayingListView;
+	private Button newGameButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_now_playing_course_screen);
+		
+		currentlyPlayingListView = (ListView) findViewById(R.id.NowPlayingCourseScreen_CurrentlyPlayingListView);
+		newGameButton = (Button) findViewById(R.id.NowPlayingCourseScreen_NewGameButton);
+		
+		newGameButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				MasterController.game = new Game(null, MasterController.currentCourse, null, null);
+				
+				
+			}
+		});
 	}
 
 	@Override
