5d5cce6ca720cf6ce15a82e82282604eb6e06c5f
thatAndyMoore <arpmoore@gmail.com>
2013-12-12 18:46:32 -0500

    Fixed hole number issue on score screen

--- eclipseClientProject/GolfrClient/src/com/golfrclient/HoleSelectionScreen.java
+++ eclipseClientProject/GolfrClient/src/com/golfrclient/HoleSelectionScreen.java
@@ -79,7 +79,7 @@ public class HoleSelectionScreen extends Activity {
 			{
 				
 				MasterController.currentHole = (Hole) holeListView.getItemAtPosition(position);
-				MasterController.currentHoleNum = MasterController.currentHole.getHoleNumber();
+				MasterController.currentHoleNum = (MasterController.currentHole.getHoleNumber()) - 1;
 				Intent i = new Intent(HoleSelectionScreen.this, ScoreEntryScreen.class);
 				startActivity(i);
 			}
